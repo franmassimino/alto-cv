e76aa5d74b2dae38739d1dc72807e3114dabc403
franmassimino <franmassi1904@gmail.com>
2025-03-13 00:00:58 +0100

    login button

--- src/components/web/header.tsx
+++ src/components/web/header.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link"
 import { ThemeToggle } from "../ui/theme-toggle"
 import { Button } from "../ui/button"
+import { auth } from "@/lib/auth/auth"
 
-export function Header() {
+export async function Header() {
+  const session = await auth()
+
   return (
     <header className="sticky top-0 z-40 w-full px-[5%] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex max-w-[1200px] mx-auto  h-16 items-center justify-between">
@@ -13,11 +16,18 @@ export function Header() {
 
         <div className="flex items-center gap-4">
           <ThemeToggle />
-          <Link href="/login">
-            <Button>
-              Iniciar Sesión
-            </Button>
-          </Link>
+          {session ?
+            <Link href="/dashboard">
+              <Button>
+                Ir al dashboard
+              </Button>
+            </Link> :
+            <Link href="/login">
+              <Button>
+                Iniciar Sesión
+              </Button>
+            </Link>
+          }
         </div>
       </div>
     </header>
